e1503bfad3de4e529a2c3657e2d76a05fcf6dfc2
Steve Kondik <steve@cyngn.com>
2015-07-11 05:49:30 -0700

    video: mdss: LiveDisplay driver
     * Bring in a lightweight version of the LiveDisplay driver which
       works for this chipset, and remove all the OEM spaghetti code.
     * This version of the driver does not include the generic
       RGB offset postprocessing, since we're going to do this using
       the official QDCM APIs.
    
    mdss: livedisplay: Allow independent CABC command
    
     * The current CABC/SRE/ACO command is OR'd together from CABC off
       command and a mode mask. However, on some modern panels, the commands
       are more complex and can't be simply calculated. Add DT fields to
       store independent CABC command without runtime calculation.
     * Try the simple unified CABC commands first, and use the independent
       one as fallback

--- drivers/video/fbdev/msm/mdss_panel.h
+++ drivers/video/fbdev/msm/mdss_panel.h
@@ -313,6 +313,9 @@ enum mdss_intf_events {
 	MDSS_EVENT_REGISTER_CLAMP_HANDLER,
 	MDSS_EVENT_DSI_DYNAMIC_BITCLK,
 	MDSS_EVENT_MAX,
+#ifdef CONFIG_FB_MSM_MDSS_LIVEDISPLAY
+	MDSS_EVENT_UPDATE_LIVEDISPLAY,
+#endif
 };
 
 /**
@@ -764,6 +767,8 @@ struct mdss_dsi_dual_pu_roi {
 	bool enabled;
 };
 
+struct mdss_livedisplay_ctx;
+
 struct mdss_panel_hdr_properties {
 	bool hdr_enabled;
 
@@ -925,6 +930,8 @@ struct mdss_panel_info {
 	 * configuring the event timer wakeup logic.
 	 */
 	u32 adjust_timer_delay_ms;
+
+	struct mdss_livedisplay_ctx *livedisplay;
 
 	/* debugfs structure for the panel */
 	struct mdss_panel_debugfs_info *debugfs_info;
